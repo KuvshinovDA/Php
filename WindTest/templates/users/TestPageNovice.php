d228d3288892df877d0cc5fa6f02683e44d60204
KuvshinovDA <kuvshinov_d_a@mail.ru>
2019-08-08 10:56:35 +0300

    Тест

---
+++ WindTest/templates/users/TestPageNovice.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Тест</title>
+</head>
+<body>
+    <h3>тест новичок</h3>
+    
+    <?php
+    // echo '<pre>';
+    // var_dump ($qwe);
+    // var_dump ($answer);
+    // foreach ($quest as $w =>$quest) {
+    //     var_dump ($quest);
+    // }
+//      foreach ($quest as $answerNum => $ques) : 
+
+    $a = '';
+echo '<pre>';
+        
+        print_r ($_SESSION['login']); 
+        echo '<pre>';
+        print_r ($_SESSION['instructor']);
+        echo '<pre>';
+        print_r ($_SESSION['level']);
+        echo '<pre>';
+        print_r ($options);          
+        echo '<pre>';
+        print_r ($name);          
+    ?>
+      
+</body>
+</html>
